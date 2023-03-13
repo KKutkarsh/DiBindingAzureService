a36b59f25bb04a5916eb86154a36bfb8ebdab296
KKutkarsh <utkarsh2188@gmail.com>
2023-03-13 21:15:25 +0000

    Http trigger as Input binding and Azure queue as output binding (#1)
    Co-authored-by: utkarsh kashyap <utkarsh.kashyap@vistatec.com>

---
+++ Models/QueueResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace AzureFunctionTester.Models
+{
+    public class QueueResponse
+    {
+        public string[] Messages { get; set; }
+        public string HttpResponse { get; set; }
+    }
+}
