40ea243ed7eede1940e0f845eae7609ca09eb4bb
KKutkarsh <utkarsh2188@gmail.com>
2023-03-15 09:35:36 +0000

    Added DI for azure queues using Factory pattern (#2)
    Co-authored-by: utkarsh kashyap <utkarsh.kashyap@vistatec.com>

---
+++ Helpers/BootstrapAzureQueues.cs
@@ -0,0 +1,22 @@
+﻿using AzureFunctionTester.Interface;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AzureFunctionTester.Helpers
+{
+    public static class BootstrapAzureQueues
+    {
+        public static IServiceCollection AddOutputQueue(this IServiceCollection services)
+        {
+            Func<IServiceProvider, IOutputQueueService> factory = (serviceProvider) =>
+            {
+                //IConfiguration configuration = serviceProvider.GetService<IConfiguration>();
+                //EnvironmentHelper environmentHelper = configuration.Get<EnvironmentHelper>();
+                return new OutputQueueService("can get it from env variable", "some constant or env variable");
+            };
+
+            return services.AddSingleton(factory);
+        }
+    }
+}
